52bb18c97cfa66949f95d87c8409c07153d46c26
pavvo <vv.pavlee@gmail.com>
2018-05-16 11:17:56 +0200

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+
+class App extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <Titles />
+                <Form />
+            </div>
+        );
+    }
+  
+}
+
+export default App;
