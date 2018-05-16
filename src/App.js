9fd67709e0b3d456a4bfb382c0a175256e5a0e91
pavvo <vv.pavlee@gmail.com>
2018-05-16 14:00:51 +0200

    Regular commit #2

--- src/App.js
+++ src/App.js
@@ -7,11 +7,44 @@ const API_KEY = 'c6a89f4435621213beeed903593f1aeb';
 
 class App extends React.Component{
 
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined,
+    }
+
     getWeather = async (e) => {
         e.preventDefault();
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,GB&appid=${API_KEY}&units=metric`);
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
         const data = await api_call.json();
-        console.log(data);
+
+        if (city && country) {
+            console.log(data);
+
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error: ""
+            });
+        } else {
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "Please enter the value"
+            });
+        }
+
     }
 
     render(){
@@ -19,7 +52,14 @@ class App extends React.Component{
             <div>
                 <Titles />
                 <Form getWeather={this.getWeather}/>
-                <Weather />
+                <Weather 
+                    temperature = {this.state.temperature}
+                    city = {this.state.city}
+                    country = {this.state.country}
+                    humidity = {this.state.humidity}
+                    description = {this.state.description}
+                    error = {this.state.error}
+                />
             </div>
         );
     }
